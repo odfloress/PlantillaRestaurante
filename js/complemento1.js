0dd837eb935f685f71bda48398c0f9924b1c73c7
odfloress <odfloress@unah.hn>
2023-08-11 17:01:20 -0600

    Modificaciones

--- js/complemento1.js
+++ js/complemento1.js
@@ -8,14 +8,3 @@ document.querySelector('#menu-btn').onclick = () =>{
 window.onscroll = () =>{
     navbar.classList.remove('active');
 }
-
-
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == navbar) {
-//     if(navbar.classList.contains( 'active' )){
-//         navbar.classList.remove('active');
-//     }
-//   }
-// }
